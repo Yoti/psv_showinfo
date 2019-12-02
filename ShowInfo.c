f67d683e05a8cddf511b424b8dd7bf9f8c6a8e37
Yoti <realyoti@gmail.com>
2019-12-02 15:28:05 +0300

    v0.6

--- ShowInfo.c
+++ ShowInfo.c
@@ -1,108 +1,122 @@
-﻿#include <taihen.h>
+﻿/*
+	ShowInfo by Yoti
+*/
+
+#include <taihen.h>
 #include <vitasdk.h>
 #include <stdio.h>
 #include "blit.h"
 
 /*
-	blit_string
-
-	X: 960px/16ppc=60chars
-	Y: 544px/16ppc=34chars
-	*: PPC = Pixels Per Char
-*/
-/*
 	sceAppMgrAppParamGetString
 
-	_6 CONTENT_ID
-	_8 CATEGORY
-	_9 TITLE
-	10 STITLE
-	12 TITLE_ID
+	Params:
+	__6 0x06 CONTENT_ID
+	__7 0x07 NP_COMMUNICATION_ID
+	__8 0x08 CATEGORY
+	__9 0x09 TITLE
+	_10 0x0a STITLE
+	_12 0x0c TITLE_ID
+	100 0x64 CONTENT_ID (same as 6)
+	101 0x65 TITLE (same as 9)
 */
 /*
 	sceAppMgrAppParamGetInt
 
-	_3 ?
-	11 SAVEDATA_MAX_SIZE?
-	13 ?
-	16 ?
+	Params:
+	_1 0x01 ???
+	_2 0x02 ???
+	_3 0x03 APP_VER
+	_4 0x04 ???
+	_5 0x05 ???
+	11 0x0b SAVEDATA_MAX_SIZE
+	13 0x0d ???
+	14 0x0e ???
+	16 0x10 ???
+
+	Errors:
+	80800009 PARAM_DOES_NOT_EXIST?
+	80801042 WRONG_PARAM_TYPE?
 */
 
-int sceAppMgrAppParamGetInt(int pid, int param, int *value); // fake proto
+int sceAppMgrAppParamGetInt(int pid, int param, int *value);
 
-int show_info = 1;
+int show_info = 0;
 static SceUID g_hooks[1];
+
+void asciify(char from[], char to[]) {
+	for (int i = 0; i < strlen(from); i++) {
+		if (from[i] <= 0x7F) {
+			if (from[i] == 0x0A) // new line
+				sprintf(to, "%s%c", to, 0x20);
+			else if (from[i] >= 0x20)
+				sprintf(to, "%s%c", to, from[i]);
+		} else if (from[i] <= 0xDF) {
+			if ((from[i] != 0xC2) && (from[i+1] != 0xAE)) // copyright
+				sprintf(to, "%s%c", to, '#');
+			i++;
+		} else if (from[i] <= 0xEF) {
+			if ((from[i] != 0xE2) && (from[i+1] != 0x84) && (from[i+2] != 0xA2)) // trademark
+				sprintf(to, "%s%c", to, '#');
+			i++;
+			i++;
+		} else if (from[i] <= 0xFF) {
+			sprintf(to, "%s%c", to, '#');
+			i++;
+			i++;
+			i++;
+		}
+	}
+}
 
 static tai_hook_ref_t ref_hook0;
 int sceDisplaySetFrameBuf0(const SceDisplayFrameBuf *pParam, int sync) {
 	if (show_info) {
 
-	int i, y = 0, x, len, num;
-	char tmp[15] = {0};
+	int i, y = 0, k = 32;
+	int len, ret, num;
+	char str[255] = {0};
 	char buf[255] = {0};
 
 	blit_set_frame_buf(pParam);
-	blit_set_color(0x00FFFFFF, 0x1F0000FF); // ABGR
-	blit_string(0, 0, "ShowInfo");
-	blit_string(16 * 56, 0, "v0.5");
+	blit_set_color(0x00FFFFFF, 0x000000FF); // ABGR
+	blit_string(0, k + 0, "ShowInfo");
+	blit_string(16 * 56, k + 0, "v0.6");
 
-	for (i = 0; i < 256; i++) {
+	for (i = 0; i < 128; i++) {
 		if (y % 2 == 0)
-			blit_set_color(0x00FFFFFF, 0x1FFF0000); // ABGR
+			blit_set_color(0x00FFFFFF, 0x00FF0000); // ABGR
 		else
-			blit_set_color(0x00FFFFFF, 0x1F0000FF); // ABGR
+			blit_set_color(0x00FFFFFF, 0x000000FF); // ABGR
 
-		memset(buf, 0, sizeof(buf));
-		sceAppMgrAppParamGetString(0, i, buf, sizeof(buf));
-		len = strlen(buf);
+		ret = 0;
+		memset(str, 0, sizeof(str));
+		ret = sceAppMgrAppParamGetString(0, i, str, sizeof(str));
+		len = strlen(str);
 
-		if (len > 0) {
-/*
+		if (ret == 0) { // string
 			memset(buf, 0, sizeof(buf));
-			sprintf(buf, "%02i|%s (%i)", i, buf, len);
-			blit_string(16 * 1, 16 + y * 16, buf);
-*/
-
-			// выводим номер строки и разделитель
-			memset(tmp, 0, sizeof(tmp));
-			sprintf(tmp, "%02i|", i);
-			blit_string(16 * 1, 16 + y * 16, tmp);
-
-			// вписываем основной текст
-			blit_string(16 * 4, 16 + y * 16, buf);
-
-			// дополняем строку точками
-			for (x = 4 + len; x < 54; x++)
-				blit_string(16 * x, 16 + y * 16, ".");
-
-			// дописываем разделитель и размер
-			memset(tmp, 0, sizeof(tmp));
-			sprintf(tmp, "|%04i", len);
-			blit_string(16 * 54, 16 + y * 16, tmp);
-
+			asciify(str, buf);
+			memset(str, 0, sizeof(str));
+			sprintf(str, "0x%02x|%s|%i", i, buf, len);
+			blit_string(16 * 1, k + 16 + y * 16, str);
 			y++;
-		} else {
-			num = 0;
-			sceAppMgrAppParamGetInt(0, i, &num);
+		}
 
-			if (num != 0) {
-				// выводим номер строки и разделитель
-				memset(tmp, 0, sizeof(tmp));
-				sprintf(tmp, "%02i|", i);
-				blit_string(16 * 1, 16 + y * 16, tmp);
+		ret = 0;
+		num = 0;
+		ret = sceAppMgrAppParamGetInt(0, i, &num);
 
-				// вписываем основной текст
-				memset(tmp, 0, sizeof(tmp));
-				sprintf(tmp, "0x%08x", num);
-				blit_string(16 * 4, 16 + y * 16, tmp);
-				
-				y++;
-			}
+		if (ret == 0) { // number
+			memset(buf, 0, sizeof(buf));
+			sprintf(buf, "0x%02x|0x%08X/%i", i, num, num);
+			blit_string(16 * 1, k + 16 + y * 16, buf);
+			y++;
 		}
 	}
 
-	blit_string(0, 16 * (y + 1), "USER");
-	blit_string(16 * 56, 16 * (y + 1), "Yoti");
+	blit_string(0, k + 16 * (y + 1), "USER");
+	blit_string(16 * 56, k + 16 * (y + 1), "Yoti");
 
 	} return TAI_CONTINUE(int, ref_hook0, pParam, sync);
 }
@@ -115,6 +129,8 @@ int module_start(SceSize argc, const void *args) {
 		TAI_ANY_LIBRARY,
 		0x7A410B64, // sceDisplaySetFrameBuf
 		sceDisplaySetFrameBuf0);
+
+	if (g_hooks[0] >= 0) show_info = 1;
 
 	return SCE_KERNEL_START_SUCCESS;
 }
